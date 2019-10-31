0a7995878677995b34e752759c34715ddd72aa49
Crikxi <crikxi.public@gmail.com>
2019-10-31 17:22:34 +0100

    fix latestButton

--- latesttweets.js
+++ latesttweets.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Latest Tweets
-// @version  1.3
+// @version  1.4
 // @grant    none
 // @include http*://twitter.com*
 // @run-at document-end
@@ -13,13 +13,6 @@ function sleep(milliseconds)
 	return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-function click(element)
-{
-	const event = document.createEvent("HTMLEvents");
-	event.initEvent("click", true, true);
-	element.dispatchEvent(event);	 
-}
-
 (async () => 
 {
 	for(i = 0; i < 5; i++)
@@ -28,17 +21,18 @@ function click(element)
 		await sleep(1000);
 
 		// Checks if tweets are set to "Home".
-		const menuButton = document.querySelector('div[aria-label="Top Tweets on"]')
+		const menuButton = document.querySelector('div[aria-label="Top Tweets on"]');
 		if (menuButton != null)
 		{
 			// Clicks on menu button.
-			click(menuButton);
-			await sleep(500);
+			menuButton.click();
+			await sleep(0);
 
 			// Click "Latest" button.
-			const latestButton = document.querySelector('div[class="css-18t94o4 css-1dbjc4n r-1loqt21 r-18u37iz r-1j3t67a r-9qu9m4 r-o7ynqc r-1j63xyz r-13qz1uu"]')
-			click(latestButton);
-			break
+			const latestButton = document.querySelector('div[role="menuitem"]');
+			latestButton.click();
+			break;
 		}
 	}
 })();
+
